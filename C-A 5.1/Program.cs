1e951977b021d100ddb5898c57fcb158b0b26cb4
Pingvu <nhlpool27@gmail.com>
2023-01-26 13:42:50 +0100

    Add project files.

---
+++ C-A 5.1/Program.cs
@@ -0,0 +1,14 @@
+﻿string[] M1 = new string[4];
+
+M1[0] = "Hej jag heter...?";
+M1[1] = "Hej jag kommer från...?";
+M1[2] = "Hej jag bor i...?";
+
+for (int i = 0; i < 1; i++)
+{
+    Console.WriteLine(M1[0]);
+    Console.WriteLine();
+    Console.WriteLine(M1[1]);
+    Console.WriteLine();
+    Console.WriteLine(M1[2]);
+}
